9dbf2c3a66f8dce7f8c4fd163a21964a5b528e67
pieterdepiet <zwartwit88@hotmail.com>
2021-08-14 11:55:21 +0200

    The experimental import function is temporarily or maybe permanently removed, and never used. Maybe there will be a time headers will be replaced with packages

--- src/include/import.h
+++ src/include/import.h
@@ -3,6 +3,12 @@
 #include "data_type.h"
 #include "scope.h"
 
+typedef struct PACKAGE_STRUCT {
+    size_t symbols_size;
+    char** symbols;
+    scope_T* package_scope;
+} package_T;
+
 void import_import_package(scope_T* scope, char* package);
 
 #endif
