06c1d2917aa28dab7bd9db3e255d5cd51c04e535
pieterdepiet <zwartwit88@hotmail.com>
2021-08-28 11:39:16 +0200

    Added integer class functions and _allocnew to create a pointer to a space for a class variable, added pointer print function for testing _allocnew and added integer class functions

--- stdlib/lib.s
+++ stdlib/lib.s
@@ -1,7 +1,9 @@
 .data
 _printi_format: .asciz "%d\n"
+_printp_format: .asciz "%p\n"
 .text
 .extern _printf
+.extern _malloc
 .globl _main
 .globl _printi
 _printi:
@@ -11,6 +13,43 @@ _printi:
     movq _printi_format@GOTPCREL(%rip), %rdi
     callq _printf
     xorl %eax, %eax
+    pop %rbp
+    ret
+_printp:
+    push %rbp
+    movq %rsp, %rbp
+    movl %edi, %esi
+    movq _printp_format@GOTPCREL(%rip), %rdi
+    callq _printf
+    xorl %eax, %eax
+    pop %rbp
+    ret
+.globl _C3Int5print
+_C3Int5print:
+    push %rbp
+    movq %rsp, %rbp
+    movl %edi, %esi
+    movl %esi, %eax
+    movq _printi_format@GOTPCREL(%rip), %rdi
+    callq _printf
+    pop %rbp
+    ret
+.globl _C3Int3add
+_C3Int3add:
+    movl %edi, %eax
+    addl %esi, %eax
+    ret
+.globl _allocnew
+_allocnew:
+    push %rbp
+    movq %rsp, %rbp
+    subq $16, %rsp
+    callq _malloc
+    movq %rax, -8(%rbp)
+    movq %rax, %rdi
+    # callq _printp
+    movq -8(%rbp), %rax
+    addq $16, %rsp
     pop %rbp
     ret
 _main:
