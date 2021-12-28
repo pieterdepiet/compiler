a145768b565a38a323d7c640f78572e28a81e104
pieterdepiet <zwartwit88@hotmail.com>
2021-12-28 14:03:29 +0100

    new global scope has function and type definitions, visitor, local scope has ptr to global and as_function. Added some functions

--- src/include/scope.h
+++ src/include/scope.h
@@ -1,24 +1,43 @@
 #ifndef SCOPE_H
 #define SCOPE_H
 #include "data_type.h"
+typedef struct GLOBAL_SCOPE_STRUCT {
+    char** type_names;
+    data_type_T** types;
+    size_t types_size;
+    char** variable_names;
+    data_type_T** variable_types;
+    size_t variables_size;
+    fspec_T** function_specs;
+    size_t functions_size;
+    struct VISITOR_STRUCT* visitor;
+} global_T;
 typedef struct SCOPE_STRUCT {
     char** variable_names;
     data_type_T** variable_types;
     size_t variables_size;
-    char** function_names;
-    fspec_T** function_specs;
-    size_t functions_size;
     int has_this;
     struct SCOPE_STRUCT* parent;
     data_type_T* return_type;
+    struct VISITOR_STRUCT* visitor;
+    struct ASSEMBLY_FUNCTION_STRUCT* as_function;
+    global_T* global;
 } scope_T;
 
+global_T* init_global_scope(struct VISITOR_STRUCT* visitor);
+scope_T* init_function_scope(global_T* parent, struct ASSEMBLY_FUNCTION_STRUCT* as_function);
 scope_T* init_scope(scope_T* parent);
 
 void scope_add_variable(scope_T* scope, char* name, data_type_T* type);
-void scope_add_function(scope_T* scope, char* name, fspec_T* fspec);
+void scope_add_type(scope_T* scope, char* name, data_type_T* type);
+void scope_add_typeg(global_T* scope, char* name, data_type_T* type);
+void scope_add_function(scope_T* scope, fspec_T* fspec);
+void scope_add_functiong(global_T* scope, fspec_T* fspec);
 data_type_T* scope_get_variable(scope_T* scope, char* name);
+data_type_T* scope_get_type(scope_T* scope, char* name);
+data_type_T* scope_get_typeg(global_T* scope, char* name);
 fspec_T* scope_get_function(scope_T* scope, char* name);
+fspec_T* scope_get_functiong(global_T* scope, char* name);
 int scope_get_variable_id(scope_T* scope, char* name);
 size_t scope_get_variable_relative_location(scope_T* scope, char* name);
 size_t scope_get_scope_size(scope_T* scope);
--- src/scope.c
+++ src/scope.c
@@ -4,12 +4,38 @@
 #include "include/list.h"
 #include <stdio.h>
 
+global_T* init_global_scope(struct VISITOR_STRUCT* visitor) {
+    global_T* scope = calloc(1, sizeof(global_T));
+    scope->variable_names = (void*) 0;
+    scope->variable_types = (void*) 0;
+    scope->variables_size = 0;
+    scope->function_specs = (void*) 0;
+    scope->functions_size = 0;
+    scope->type_names = (void*) 0;
+    scope->types = (void*) 0;
+    scope->types_size = 0;
+    scope->visitor = visitor;
+    return scope;
+}
+scope_T* init_function_scope(global_T* parent, struct ASSEMBLY_FUNCTION_STRUCT* as_function) {
+    scope_T* scope = calloc(1, sizeof(struct SCOPE_STRUCT));
+    scope->global = parent;
+    scope->visitor = parent->visitor;
+    scope->as_function = as_function;
+    scope->return_type = (void*) 0;
+    scope->variable_names = (void*) 0;
+    scope->variable_types = (void*) 0;
+    scope->variables_size = 0;
+    return scope;
+}
+
 scope_T* init_scope(scope_T* parent) {
     scope_T* scope = calloc(1, sizeof(struct SCOPE_STRUCT));
     scope->parent = parent;
-    if (parent) {
-        scope->return_type = parent->return_type;
-    }
+    scope->global = parent->global;
+    scope->visitor = parent->visitor;
+    scope->as_function = parent->as_function;
+    scope->return_type = parent->return_type;
     scope->variable_names = (void*) 0;
     scope->variable_types = (void*) 0;
     return scope;
@@ -20,10 +46,21 @@ void scope_add_variable(scope_T* scope, char* name, data_type_T* type) {
     scope->variables_size--;
     list_add((void***) &scope->variable_names, &scope->variables_size, name);
 }
-void scope_add_function(scope_T* scope, char* name, fspec_T* fspec) {
+void scope_add_function(scope_T* scope, fspec_T* fspec) {
+    list_add((void***) &scope->global->function_specs, &scope->global->functions_size, fspec);
+}
+void scope_add_functiong(global_T* scope, fspec_T* fspec) {
     list_add((void***) &scope->function_specs, &scope->functions_size, fspec);
-    scope->functions_size--;
-    list_add((void***) &scope->function_names, &scope->functions_size, name);
+}
+void scope_add_type(scope_T* scope, char* name, data_type_T* type) {
+    list_add((void***) &scope->global->types, &scope->global->types_size, type);
+    scope->global->types_size--;
+    list_add((void***) &scope->global->type_names, &scope->global->types_size, name);
+}
+void scope_add_typeg(global_T* scope, char* name, data_type_T* type) {
+    list_add((void***) &scope->types, &scope->types_size, type);
+    scope->types_size--;
+    list_add((void***) &scope->type_names, &scope->types_size, name);
 }
 data_type_T* scope_get_variable(scope_T* scope, char* name) {
     for (size_t i = 0; i < scope->variables_size; i++) {
@@ -36,9 +73,33 @@ data_type_T* scope_get_variable(scope_T* scope, char* name) {
     }
     return (void*) 0;
 }
+data_type_T* scope_get_type(scope_T* scope, char* name) {
+    for (size_t i = 0; i < scope->global->types_size; i++) {
+        if (utils_strcmp(name, scope->global->type_names[i])) {
+            return scope->global->types[i];
+        }
+    }
+    return (void*) 0;
+}
+data_type_T* scope_get_typeg(global_T* scope, char* name) {
+    for (size_t i = 0; i < scope->types_size; i++) {
+        if (utils_strcmp(name, scope->type_names[i])) {
+            return scope->types[i];
+        }
+    }
+    return (void*) 0;
+}
 fspec_T* scope_get_function(scope_T* scope, char* name) {
+    for (size_t i = 0; i < scope->global->functions_size; i++) {
+        if (utils_strcmp(name, scope->global->function_specs[i]->name)) {
+            return scope->global->function_specs[i];
+        }
+    }
+    return (void*) 0;
+}
+fspec_T* scope_get_functiong(global_T* scope, char* name) {
     for (size_t i = 0; i < scope->functions_size; i++) {
-        if (utils_strcmp(name, scope->function_names[i])) {
+        if (utils_strcmp(name, scope->function_specs[i]->name)) {
             return scope->function_specs[i];
         }
     }
@@ -54,7 +115,7 @@ int scope_get_variable_id(scope_T* scope, char* name) {
 }
 size_t scope_get_variable_relative_location(scope_T* scope, char* name) {
     size_t curr_location = 0;
-    if (scope->parent->parent != (void*) 0) {
+    if (scope->parent != (void*) 0) {
         curr_location = scope_get_scope_size(scope->parent);
     }
     for (size_t i = 0; i < scope->variables_size; i++) {
@@ -63,15 +124,15 @@ size_t scope_get_variable_relative_location(scope_T* scope, char* name) {
             return curr_location;
         }
     }
-    if (scope->parent->parent) {
+    if (scope->parent) {
         return scope_get_variable_relative_location(scope->parent, name);
     } else {
-        return -1;
+        return 0;
     }
 }
 size_t scope_get_scope_size(scope_T* scope) {
     size_t size = 0;
-    if (scope->parent->parent != (void*) 0) {
+    if (scope->parent != (void*) 0) {
         size = scope_get_scope_size(scope->parent);
     }
     for (size_t i = 0; i < scope->variables_size; i++) {
