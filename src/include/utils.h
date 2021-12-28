94b1691b0cc31a2c344de0bbad6a96716658bcb2
pieterdepiet <zwartwit88@hotmail.com>
2021-12-28 13:54:44 +0100

    added const qualifier

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-int utils_strcmp(char* str1, char* str2);
+int utils_strcmp(const char* str1, const char* str2);
 
-void utils_strcat(char** dest, char* src);
+void utils_strcat(char** dest, const char* src);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-int utils_strcmp(char* str1, char* str2) {
+int utils_strcmp(const char* str1, const char* str2) {
     return strcmp(str1, str2) == 0 && strlen(str1) == strlen(str2);
 }
-void utils_strcat(char** dest, char* src) {
+void utils_strcat(char** dest, const char* src) {
     *dest = realloc(*dest, (strlen(*dest) + strlen(src) + 1) * sizeof(char));
     strcat(*dest, src);
 }
